97e652b63f0cd109a4b8ae06aef387147f45cac7
Robert Ancell <robert.ancell@canonical.com>
2016-01-14 22:41:11 +1300

    Add an ability for plugins to request authorization for posting reviews

--- src/gs-plugin.h
+++ src/gs-plugin.h
@@ -117,6 +117,7 @@ typedef enum {
 
 typedef const gchar	*(*GsPluginGetNameFunc)		(void);
 typedef const gchar	**(*GsPluginGetDepsFunc)	(GsPlugin	*plugin);
+typedef const gchar	*(*GsPluginGetReviewAuthFunc)	(GsPlugin	*plugin);
 typedef void		 (*GsPluginFunc)		(GsPlugin	*plugin);
 typedef gboolean	 (*GsPluginSearchFunc)		(GsPlugin	*plugin,
 							 gchar		**value,
